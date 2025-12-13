305c5e0149837be7375c60da86df6462cf2a5972
hediye <hediyehghafari80@gmail.com>
2025-12-13 16:56:33 +0330

    update updateCurrentUser to consume authService

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -1,12 +1,12 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
-import { updateCurrentUser } from "../../services/apiAuth";
+import authService from "../../services/apiAuth";
 
 export function useUpdateUser() {
   const queryClient = useQueryClient();
 
   const { mutate: updateUser, isLoading: isUpdating } = useMutation({
-    mutationFn: updateCurrentUser,
+    mutationFn: authService.updateCurrentUser,
     onSuccess: ({ user }) => {
       toast.success("User account successfully updated");
       queryClient.setQueryData(["user"], user);
