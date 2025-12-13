e21475e5d6c5ebdd08c0aafec6a574c2477a7488
hediye <hediyehghafari80@gmail.com>
2025-12-13 16:55:14 +0330

    update signupApi to consume authService

--- src/features/authentication/useSignup.js
+++ src/features/authentication/useSignup.js
@@ -1,10 +1,10 @@
 import { useMutation } from "@tanstack/react-query";
-import { signup as signupApi } from "../../services/apiAuth";
+import authService from "../../services/apiAuth";
 import { toast } from "react-hot-toast";
 
 export function useSignup() {
   const { mutate: signup, isLoading } = useMutation({
-    mutationFn: signupApi,
+    mutationFn: authService.signupApi,
     onSuccess: (user) => {
       toast.success(
         "Account successfully created! Please verufy the new account from the user's email address."
